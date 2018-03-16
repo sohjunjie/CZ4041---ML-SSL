de22acb3e8dd49883c04321723946466b9d70a8f
sohjunjie <junjie.soh93@gmail.com>
2018-03-16 21:35:07 +0800

    print stats

--- dual-learning-ssl.py
+++ dual-learning-ssl.py
@@ -1,7 +1,14 @@
+import os
+
 from sklearn.kernel_ridge import KernelRidge
 from numpy.linalg import inv
 import numpy as np
 import pandas as pd
+import pickle
+import math
+
+RESULT_DIR = "result"
+RISK_THRESHOLD = 0.75
 
 
 def calculate_risk(real_x, reco_x, predict_correct, sigma):
@@ -10,10 +17,12 @@ def calculate_risk(real_x, reco_x, predict_correct, sigma):
         risk = np.exp(-raw_risk)
     else:
         risk = np.exp(raw_risk)
+    if risk > 999:
+        risk = 999
     return risk
 
 
-if __name__ == "__main__":
+def label_unlabeled_training_dataset_for_ssl():
 
     tra_csv = pd.read_csv('data/nursery-ssl10-10-1tra.csv')
     trs_csv = pd.read_csv('data/nursery-ssl10-10-1trs.csv')
@@ -41,6 +50,7 @@ if __name__ == "__main__":
     tra_dummy_x_labeled = tra_dummy_x.loc[tra_dummy_x.index.isin(tra_dummy_y_labeled.index)]
 
     tra_dummy_y_unlabeled = tra_dummy_y.loc[tra_dummy_y['unlabeled'] == 1]
+    tra_dummy_y_unlabeled_y = trs_dummy_y.loc[trs_dummy_y.index.isin(tra_dummy_y_unlabeled.index)]
     tra_dummy_x_unlabeled = tra_dummy_x.loc[tra_dummy_x.index.isin(tra_dummy_y_unlabeled.index)]
 
     del tra_csv
@@ -73,14 +83,28 @@ if __name__ == "__main__":
     Id = np.identity(len(XTX))
     alpha_k = np.matmul(np.matmul(inv(XTX + gamma * Id), XT), y)
 
-    tra_dummy_x_unlabeled['risk'] = 999
-    tra_dummy_y_unlabeled[:] = 0
-    for i in range(len(trs_dummy_x)):
+    # sigma values representing L2 norm distance of Xfeatures for each yClass
+    sigma_list = []
+    for c in tra_dummy_y_labeled.columns:
+        sigma = np.linalg.norm(tra_dummy_x_labeled.loc[tra_dummy_x_labeled.index.isin(tra_dummy_y_labeled.loc[tra_dummy_y_labeled[c] == 1].index)].describe().loc['std', :].as_matrix())
+        if math.isnan(sigma):
+            sigma = np.linalg.norm(tra_dummy_x_labeled.describe().loc['std', :].as_matrix())
+        sigma_list.append(sigma)
+    # sigma_list = []
+    # for c in trs_dummy_y.columns:
+    #     sigma = np.linalg.norm(trs_dummy_x.loc[trs_dummy_x.index.isin(trs_dummy_y.loc[trs_dummy_y[c] == 1].index)].describe().loc['std', :].as_matrix())
+    #     if math.isnan(sigma):
+    #         sigma = np.linalg.norm(trs_dummy_x.describe().loc['std', :].as_matrix())
+    #     sigma_list.append(sigma)
 
-        # Xrs = trs_dummy_x.as_matrix()[i]
-        # yrs = trs_dummy_y.as_matrix()[i]
-        Xrs = tra_dummy_x_unlabeled.as_matrix()[i]
-        yrs = tra_dummy_y_unlabeled.as_matrix()[i]
+    num_ssl_classify_wrong = 0
+    num_ssl_classify_correct = 0
+    num_ssl_rejected_wrong = 0
+    num_ssl_rejected_correct = 0
+    for i in tra_dummy_x_unlabeled.index:
+
+        Xrs = tra_dummy_x_unlabeled.loc[i].as_matrix()
+        yrs = tra_dummy_y_unlabeled_y.loc[i].as_matrix()
 
         prediction = clf.predict(np.reshape(Xrs, (1, len(Xrs))))
         predicted_class = np.argmax(prediction[0])
@@ -92,45 +116,63 @@ if __name__ == "__main__":
         # predict y base on reconstructed X
         prediction_reco = clf.predict(np.reshape(reco_x, (1, len(Xrs))))
         predicted_class_reco = np.argmax(prediction_reco[0])
+        sigma = sigma_list[predicted_class]
 
         # yXpredicted should be == yXreconstructed
         predict_correct = (predicted_class == predicted_class_reco)
-        calc_risk = calculate_risk(real_x, reco_x, predict_correct, 0.05)
-        tra_dummy_x_unlabeled.loc[i]['risk'] = calc_risk
-        print("predicted class: " + str(predicted_class))
-        print("predicted reconstructed class: " + str(predicted_class_reco))
-        print("real class: " + str(np.argmax(yrs)))
-        print("Risk: " + str(calc_risk))
-        # set predicted class of unlabeled if calc_risk < threshold
-        tra_dummy_y_unlabeled.loc[i][y.columns[predicted_class]] = 1
-        input("===============Enter to continue===============")
+        calc_risk = calculate_risk(real_x, reco_x, predict_correct, sigma)
+        real_class = np.argmax(yrs)
+
+        # over risk threshold is considered unsafe for ssl training
+        if calc_risk > RISK_THRESHOLD:
+            if real_class == predicted_class:
+                num_ssl_rejected_wrong += 1
+            if real_class != predicted_class:
+                num_ssl_rejected_correct += 1
+
+        # over risk threshold is considered safe for ssl training
+        if calc_risk < RISK_THRESHOLD:
+            if real_class == predicted_class:
+                num_ssl_classify_correct += 1
+            if real_class != predicted_class:
+                num_ssl_classify_wrong += 1
+                # better to add these wrongly predicted classes as supervised training
+                # print("i: " + str(i))
+                # print("predicted class: " + str(predicted_class))
+                # print("predicted reconstructed class: " + str(predicted_class_reco))
+                # print("real class: " + str(real_class))
+                # print("Risk: " + str(calc_risk))
+
+    print("num classified wrong: " + str(num_ssl_classify_wrong))
+    print("num classified correct: " + str(num_ssl_classify_correct))
+    print("num instances incorrectly rejected: " + str(num_ssl_rejected_wrong))
+    print("num instances correctly rejected: " + str(num_ssl_rejected_correct))
+
+    merge_tra_dummy_y = pd.concat([tra_dummy_y_labeled, tra_dummy_y_unlabeled])
+    merge_tra_dummy_x = pd.concat([tra_dummy_x_labeled, tra_dummy_x_unlabeled])
+
+    # data_path = os.path.join(os.getcwd(), RESULT_DIR, 'dataset-x-ssl-risked.pickle')
+    # with open(data_path, 'wb') as f:
+    #     pickle.dump(merge_tra_dummy_x, f)
+    #
+    # data_path = os.path.join(os.getcwd(), RESULT_DIR, 'dataset-y-ssl-risked.pickle')
+    # with open(data_path, 'wb') as f:
+    #     pickle.dump(merge_tra_dummy_y, f)
+
+    return merge_tra_dummy_x, merge_tra_dummy_y
 
 
-######################################
-# PRIMAL FORM RLS
-# n_samples, n_features = 10, 5
-# rng = np.random.RandomState(0)
-# y = rng.uniform(low=0, high=1, size=[n_samples, 3])
-# y = np.where(y > 0.5, 1, 0)
-# X = rng.uniform(low=0, high=1, size=[n_samples, n_features])
-#
-# clf = KernelRidge(alpha=0.05)
-# clf.fit(X, y)
-#
-# gamma = 0.05
-# K = clf._get_kernel(X)
-# Id = np.identity(len(X))
-#
-# # Equation 4
-# alpha_star = np.matmul(inv(K + gamma * (Id)), y)
-#
-#
-# # DUAL FORM CRC
-# XT = np.transpose(X)
-# XTX = np.matmul(XT, X)
-#
-# Id = np.identity(len(XTX))
-# alpha_k = np.matmul(np.matmul(inv(XTX + gamma * Id), XT), y)
-#
-# prediction = np.matmul(alpha_k, res)
-# print(prediction)
+if __name__ == "__main__":
+
+    # check if labeling of unlabeled training dataset is done
+    labeled_tra_x_exists = os.path.exists(os.path.join(os.getcwd(), RESULT_DIR, 'dataset-x-ssl-risked.pickle'))
+    labeled_tra_y_exists = os.path.exists(os.path.join(os.getcwd(), RESULT_DIR, 'dataset-y-ssl-risked.pickle'))
+
+    if not (labeled_tra_x_exists and labeled_tra_y_exists):
+        tra_dummy_x, tra_dummy_y = label_unlabeled_training_dataset_for_ssl()
+    else:
+        with open(os.path.join(os.getcwd(), RESULT_DIR, 'dataset-x-ssl-risked.pickle'), 'rb') as f:
+            tra_dummy_x = pickle.load(f)
+
+        with open(os.path.join(os.getcwd(), RESULT_DIR, 'dataset-y-ssl-risked.pickle'), 'rb') as f:
+            tra_dummy_y = pickle.load(f)
